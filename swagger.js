5fcb421d7c333f2e202b8743ac483c4a59d0054c
Chris Thomas <ccthomas@anl.gov>
2025-03-23 06:48:22 -0500

    feat: added components.schemas from Solr managed-schema files that are now referenced as models in the spec

--- swagger.js
+++ swagger.js
@@ -1,6 +1,9 @@
 const swaggerJSDoc = require("swagger-jsdoc");
 const fs = require("fs");
 const yaml = require("yaml");
+const xml2js = require('xml2js');
+const glob = require('glob');
+const path = require('path');
 
 const getTagFromPath = (routePath) => {
   const segments = routePath.split('/');
@@ -44,6 +47,54 @@ const generateJsDoc = (method, routePath) => {
 
 const swaggerSpec = swaggerJSDoc(options);
 
-fs.writeFileSync("./swagger-output.yaml", yaml.stringify(swaggerSpec));
+const SOLR_DIR = path.join(__dirname, '..', 'bvbrc-solr');
+const schemaFiles = glob.sync(`${SOLR_DIR}/*/managed-schema`);
+const parser = new xml2js.Parser();
+
+swaggerSpec.components = swaggerSpec.components || {};
+swaggerSpec.components.schemas = swaggerSpec.components.schemas || {};
+
+const solrTypeToOpenAPI = {
+  string: 'string',
+  boolean: 'boolean',
+  int: 'integer',
+  float: 'number',
+  double: 'number',
+  long: 'integer',
+  tdate: 'string',
+  text_general: 'string'
+};
+
+const parseFields = async (filePath) => {
+  const xml = fs.readFileSync(filePath, 'utf8');
+  const result = await parser.parseStringPromise(xml);
+  const fields = result.schema.field || [];
+  const properties = {};
+
+  fields.forEach(f => {
+    const name = f.$.name;
+    const type = solrTypeToOpenAPI[f.$.type] || 'string';
+    properties[name] = { type };
+  });
+
+  return properties;
+};
+
+const injectSolrSchemas = async () => {
+  for (const file of schemaFiles) {
+    const name = path.basename(path.dirname(file));
+    const properties = await parseFields(file);
+
+    swaggerSpec.components.schemas[name] = {
+      type: 'object',
+      properties
+    };
+  }
+};
+
+(async () => {
+  await injectSolrSchemas();
+  fs.writeFileSync("./swagger-output.yaml", yaml.stringify(swaggerSpec));
+})();
 
 module.exports = swaggerSpec;
